aeb270dcd8f3ed0bdd437ad5af523c81da8f9ee1
GunBBB <yn962271@gmail.com>
2022-03-12 20:12:49 +0900

    [JAVA] LeapYear.java 코드 작성
    2024년이 윤년이면 True 그렇지 않으면 False를 출력하는 코드를 작성

---
+++ src/LeapYear.java
@@ -0,0 +1,14 @@
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year =2024;
+		
+		boolean leapyear;
+		
+		leapyear = (2024 % 4 == 0);
+		System.out.prinln("2024 는 윤년인가:" +leapyear);
+	}
+
+}
