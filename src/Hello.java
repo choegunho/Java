89320c43f9a3c5ee1f8d3d826b40738a9564376e
GunBBB <yn962271@gmail.com>
2022-03-12 20:10:52 +0900

    [JAVA] Hello.java 코드 작성
    프로젝트 환경을 구성하고 첫코드

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * 첫 번째 예제 프로그램
+ * @author 최건호
+ * @version 1.0 2015-08-01
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!"); // "Hello World!" 를 출력합니다.
+		System.out.println("I'm a new Java Programmer.");
+	}
+
+}
