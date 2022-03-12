1f406f3074ab282df4c150adefbac7767801bda4
GunBBB <yn962271@gmail.com>
2022-03-12 20:11:57 +0900

    [JAVA] AreaTest.java 코드 작성
    반지름이 3.0인 원의 둘레를 구하는 코드 작성

---
+++ src/AreaTest.java
@@ -0,0 +1,13 @@
+
+public class AreaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double dRadius = 3.0;
+		final double dPI = 3.141592;
+		double dArea = dPI * dRadius * dRadius;
+		
+		System.out.println("반지름이"+dRadius+"인 원의 면적은"+dArea+"입니다.");
+	}
+
+}
