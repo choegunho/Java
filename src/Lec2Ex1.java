61d243cc6a330a20d0568147b8259ad8e1da09e2
GunBBB <yn962271@gmail.com>
2022-03-20 11:58:31 +0900

    [Java] Lec2Ex1.java 코드 작성
    두 정수를 입력받아 둘중 더 큰 수를 출력하는 코드 작성

---
+++ src/Lec2Ex1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Lec2Ex1 {
+
+	public static void main(String[] args) {
+		int num1,num2;
+		Scanner input = new Scanner(System.in);
+		System.out.print("두개의 정수를 입력하세요: ");
+		num1 = input.nextInt();
+		num2 = input.nextInt();
+		
+		if(num1 > num2)
+			System.out.println("더 큰 정수는"+num1+"입니다.");
+		else
+			System.out.println("더 큰 정수는"+num2+"입니다.");
+	}
+
+}
