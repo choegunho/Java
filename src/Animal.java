33161518020183e5b9c40cd526767f4a8a68120b
GunBBB <yn962271@gmail.com>
2022-04-20 22:22:09 +0900

    [java] 7주차 코드 작성
    상속을 이용하여 코드를 작성

---
+++ src/Animal.java
@@ -0,0 +1,17 @@
+class Animal {
+	public void sound() {
+		System.out.println("家府家府");
+	}
+}
+
+class Dog {
+	public void sound() {
+		System.out.println("港港");
+	}
+}
+
+class Cat {
+	public void sound() {
+		System.out.println("具克");
+	}
+}
---
+++ src/AnimalTest.java
@@ -0,0 +1,14 @@
+public class AnimalTest {
+
+	public static void main(String[] args) {
+		
+		Dog	a1 = new Dog();
+		Cat	a2 = new Cat();
+		
+		a1.sound();
+		a2.sound();
+		
+		()->system.out.println("Hello World!");
+	}
+
+}
