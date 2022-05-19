cc5b8473235ba08907b8fb6cc788dffd7772fe4c
GunBBB <yn962271@gmail.com>
2022-05-19 21:15:33 +0900

    [java] 10주차 코드 작성
    액션, 키, 마우스등을 이용하여 이벤트를 구동하는 코드를 작성

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -3,25 +3,37 @@ import javax.swing.*;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.*;
 
 public class MyFrame extends JFrame{
+	private JButton button;
+	private JLabel label;
 	
 	public MyFrame() {
-		Toolkit kit = Toolkit.getDefaultToolkit();
-		Dimension screenSize = kit.getScreenSize();
-		setSize(300, 200);
-		setLocation(screenSize.width/2,screenSize.height/2);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("MyFrame");
-		Image img = kit.getImage("icon.gif");
-		setIconImage(img);
-		setVisible(true);
+		this.setSize(300, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("이벤트 예제");
 		
-		setLayout(new FlowLayout());//배치 관리자 설정!
-		JButton button = new JButton("버튼");	//컴포넌트 생성 및 추가
-		this.add(button);
-		setVisible(true);
-	}
+		JPanel panel = new JPanel();
+		button = new JButton("버튼을 누르시오");
+		label = new JLabel("아직 버튼이 눌려지지 않았습니다.");
+		button.addActionListener(e->{
+			label.setText("마침내 버튼이 눌려졌습니다.");
+		});
+		panel.add(button);
+		panel.add(label);
+		this.add(panel);
+		this.setVisible(true);
+	}	
+	/*private class MyListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == button) {
+				label.setText("마침내 버튼이 눌려졌습니다.");
+			}
+		}
+	}*/
+
 }
 
